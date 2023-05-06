5b76efc9b4058f5f9822616e8a848fede1083c53
Rony Richard <rony.richard11@gmail.com>
2023-05-06 17:21:00 -0600

    Working. Needs Week03 Requirements.

--- Main.cpp
+++ Main.cpp
@@ -1,83 +1,23 @@
 //
-//  Week02.cpp
-//  Second Week Learning C++ Assignment
+//  Main.cpp
+//  Third Week Learning C++ Assignment
 //
-//  Created by Rony Richard on 28st April, 2023
+//  Created by Rony Richard on 6th May, 2023
 //
-//  Goal: To Replicate a simple ATM Program using Classes
+//  Goal: To Replicate a simple ATM Program using more advanced systems
 
 // Libraries and directives for Windows/Linux-based machines necessary to compile this program
+
 #ifdef _WIN32
   #define _CRT_SECURE_NO_WARNINGS
 #endif /* _WIN32 */
 
 #include <iostream> // for cout, cin
 #include <string>   // for string
+#include "Account.hpp"
 using namespace std;
 
-class BankAccount{
-    private:
 
-    int accountID;
-    string accountName;
-    float accountBalance;
-
-    public:
-    // Constructor
-    BankAccount(int id, string name, float balance){
-        accountID = id;
-        accountName = name;
-        accountBalance = balance;
-    }
-
-
-    // Accessor Methods
-    int getAccountID() {
-        return accountID;
-    }
-
-    string getAccountName() {
-        return accountName;
-    }
-
-    float getAccountBalance() {
-        return accountBalance;
-    }
-
-    // Mutator methods
-    void setAccountID(int id) {
-        accountID = id;
-    }
-
-    void setAccountName(string name) {
-        accountName = name;
-    }
-
-    void setAccountBalance(float balance) {
-        accountBalance = balance;
-    }
-
-    // Deposit method
-    void deposit(float amount) {
-        accountBalance += amount;
-    }
-
-    // Withdrawal method
-    void withdraw(float amount) {
-        if (amount <= accountBalance) {
-            accountBalance -= amount;
-        } else {
-            cout << "Insufficient funds!" << endl;
-        }
-    }
-
-    // Print account information
-    void printAccountInfo() {
-        cout << "Account ID: " << accountID << endl;
-        cout << "Account Name: " << accountName << endl;
-        cout << "Account Balance: $" << accountBalance << endl;
-    }
-};
 
 int main(){
     int id_num = rand() % 99999 + 13427;
